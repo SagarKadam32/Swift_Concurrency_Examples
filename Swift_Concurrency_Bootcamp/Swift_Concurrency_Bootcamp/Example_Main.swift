0398fb89626b9869b15054be53930eb76a5ab284
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-10-03 22:31:56 +0530

    Added the view layer for the formats

---
+++ Swift_Concurrency_Bootcamp/Swift_Concurrency_Bootcamp/Example_Main.swift
@@ -0,0 +1,61 @@
+//
+//  Example_Main.swift
+//  Swift_Concurrency_Bootcamp
+//
+//  Created by Sagar Kadam on 03/10/23.
+//
+
+import SwiftUI
+
+struct Example_Main: View {
+    var body: some View {
+        Group {
+            NavigationLink(destination: E1_DoCatchTryThrowsBootcamp()) {
+                Text("1. How to use Do, Try, Catch, and Throws in Swift")
+            }
+            
+        }
+    }
+}
+
+struct SectionMainView<Content: View> : View {
+    var sectionTitle = ""
+    var sectionTitleDescription = ""
+    @ViewBuilder var content : Content
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            SectionHeader(sectionTitle:sectionTitle, sectionTitleDescription: sectionTitleDescription)
+            content
+                .frame(maxWidth: .infinity,alignment: .leading)
+        }
+        Divider()
+    }
+}
+
+struct SectionHeader : View {
+    var sectionTitle = ""
+    var sectionTitleDescription = ""
+
+    var body: some View {
+        VStack(spacing:0) {
+            Text(sectionTitle)
+                .frame(maxWidth: .infinity, maxHeight: 35, alignment: .leading)
+                .padding()
+                .font(.title)
+                .background(.mint)
+            Text(sectionTitleDescription)
+                .frame(maxWidth: .infinity, minHeight: 15, maxHeight: 25, alignment: .leading)
+                .padding()
+                .font(.headline.bold())
+                .background(.yellow)
+        }
+
+    }
+}
+
+struct Example_Main_Previews: PreviewProvider {
+    static var previews: some View {
+        Example_Main()
+    }
+}
--- Swift_Concurrency_Bootcamp/Swift_Concurrency_Bootcamp/Swift_Concurrency_BootcampApp.swift
+++ Swift_Concurrency_Bootcamp/Swift_Concurrency_Bootcamp/Swift_Concurrency_BootcampApp.swift
@@ -11,7 +11,23 @@ import SwiftUI
 struct Swift_Concurrency_BootcampApp: App {
     var body: some Scene {
         WindowGroup {
-            E1_DoCatchTryThrowsBootcamp()
+            NavigationView {
+                ScrollView {
+                    ScrollViewReader { value in
+                        
+                        VStack(spacing: 20) {
+                            Group {
+                                
+                                SectionMainView(sectionTitle: "Swift Concurrency", sectionTitleDescription: "Swift Concurrency is a major upgrade to the Swift language that completely changes how to write asynchronous code in Swift. This includes taking a deep dive into Async, Await, Actors, Tasks and more! These new features are INCREDIBLY powerful and I will be using them in all of my apps going forward!") {
+                                    Example_Main()
+                                }
+                             }
+                            .id(0)
+                        }
+                    }
+                    .padding()
+                 }
+            }
         }
     }
 }
