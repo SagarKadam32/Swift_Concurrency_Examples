f87bd6166b4f6c8445be5b7d79ea5577d21fe94b
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-10-05 00:54:32 +0530

    Initial code for Download Image - with Closure

---
+++ Swift_Concurrency_Bootcamp/Swift_Concurrency_Bootcamp/E2_DownloadImageAsync.swift
@@ -0,0 +1,64 @@
+//
+//  E2_DownloadImageAsync.swift
+//  Swift_Concurrency_Bootcamp
+//
+//  Created by Sagar Kadam on 04/10/23.
+//
+
+import SwiftUI
+
+class DownloadImageAsyncImageLoader {
+    
+    let url = URL(string: "https://picsum.photos/200")!
+    
+    func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let data = data,
+                let image = UIImage(data: data),
+                let response = response as? HTTPURLResponse,
+                response.statusCode <= 200 && response.statusCode < 300 else {
+                completionHandler(nil, error)
+                return
+            }
+            completionHandler(image,nil)
+        }
+        .resume()
+    }
+}
+
+class DownloadImageViewModel : ObservableObject {
+    
+    @Published var image : UIImage? = nil
+    let loader = DownloadImageAsyncImageLoader()
+    
+    func fetchImage() {
+        self.image = UIImage(systemName: "heart.fill")
+    }
+    
+}
+
+struct E2_DownloadImageAsync: View {
+    
+    @StateObject private var viewModel = DownloadImageViewModel()
+    
+    var body: some View {
+        ZStack {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+            }
+        }
+        .onAppear {
+            viewModel.fetchImage()
+        }
+    }
+}
+
+struct E2_DownloadImageAsync_Previews: PreviewProvider {
+    static var previews: some View {
+        E2_DownloadImageAsync()
+    }
+}
