1fb7163da9da444e50347b7bda0eb4639d746b9b
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-10-05 01:33:39 +0530

    Image Download Using Combine

--- Swift_Concurrency_Bootcamp/Swift_Concurrency_Bootcamp/E2_DownloadImageAsync.swift
+++ Swift_Concurrency_Bootcamp/Swift_Concurrency_Bootcamp/E2_DownloadImageAsync.swift
@@ -6,31 +6,48 @@
 //
 
 import SwiftUI
+import Combine
 
 class DownloadImageAsyncImageLoader {
     
     let url = URL(string: "https://picsum.photos/200")!
     
+    func handleResponse(data: Data?, response: URLResponse?) -> UIImage? {
+        guard
+            let data = data,
+            let image = UIImage(data: data),
+            let response = response as? HTTPURLResponse,
+            response.statusCode <= 200 && response.statusCode < 300 else {
+            return nil
+        }
+        return image
+    }
+    
     func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
-        URLSession.shared.dataTask(with: url) { data, response, error in
-            guard
-                let data = data,
-                let image = UIImage(data: data),
-                let response = response as? HTTPURLResponse,
-                response.statusCode <= 200 && response.statusCode < 300 else {
-                completionHandler(nil, error)
-                return
-            }
-            completionHandler(image,nil)
+        URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
+            let image = self?.handleResponse(data: data, response: response)
+            completionHandler(image,error)
         }
         .resume()
+    }
+    
+    func downloadWithCombine() -> AnyPublisher<UIImage?, Error> {
+        URLSession.shared.dataTaskPublisher(for: url)
+            .map(handleResponse)
+            .mapError({ $0 })
+            .eraseToAnyPublisher()
     }
 }
 
 class DownloadImageViewModel : ObservableObject {
     
     @Published var image1 : UIImage? = nil
+    @Published var image2 : UIImage? = nil
+    @Published var image3 : UIImage? = nil
+
+    
     let loader = DownloadImageAsyncImageLoader()
+    var cancellables = Set<AnyCancellable>()
     
     func fetchImage() {
         loader.downloadWithEscaping { [weak self] image, error in
@@ -40,6 +57,16 @@ class DownloadImageViewModel : ObservableObject {
                 }
             }
         }
+        
+        loader.downloadWithCombine()
+            .receive(on: DispatchQueue.main)
+            .sink { _ in
+                
+            } receiveValue: { [weak self] image in
+                self?.image2 = image
+            }
+            .store(in: &cancellables)
+
     }
     
 }
@@ -55,6 +82,18 @@ struct E2_DownloadImageAsync: View {
                     .bold()
                     
                 if let image = viewModel.image1 {
+                    Image(uiImage: image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 250, height: 250)
+                }
+            }
+            
+            VStack {
+                Text("Image Downlaod Using Combine :")
+                    .bold()
+                    
+                if let image = viewModel.image2 {
                     Image(uiImage: image)
                         .resizable()
                         .scaledToFit()
