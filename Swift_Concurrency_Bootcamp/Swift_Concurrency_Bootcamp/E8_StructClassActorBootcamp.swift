ada5152c7e18f25ebd75bccebb4e1f64d20a894f
Sagar Kadam <misc.sagarkadam@gmail.com>
2023-11-11 08:57:38 +0530

    Struct in Detail
    1. In case of the Structures we pass the values of the one structure to another.
    2. The new structure is completely new and separate from the previous structure when one structure is copied to other.

--- Swift_Concurrency_Bootcamp/Swift_Concurrency_Bootcamp/E8_StructClassActorBootcamp.swift
+++ Swift_Concurrency_Bootcamp/Swift_Concurrency_Bootcamp/E8_StructClassActorBootcamp.swift
@@ -16,9 +16,34 @@ struct E8_StructClassActorBootcamp: View {
     }
 }
 
+struct MyStruct {
+    var title: String
+}
+
 extension E8_StructClassActorBootcamp {
     
     private func runTest() {
         print("Test started..")
+        structTest1()
+
+    }
+    
+    private func structTest1() {
+        let objectA = MyStruct(title: "Starting Title.")
+        print("Object A: ", objectA.title)
+        
+        print("Pass VALUES of Object-A TO Object-B")
+        var objectB = objectA
+        print("Object B: ", objectB.title)
+        
+        objectB.title = "Second Title!"
+        
+        print("Object B Title changed!")
+        print("Object A: ", objectA.title)
+        print("Object B: ", objectB.title)
+
+        
+        
+        
     }
 }
